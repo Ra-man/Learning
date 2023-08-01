262af5a08f37ed0b3af1727d8d972651191d1911
Ra-man <rafaelmarquespagnan@gmail.com>
2023-08-01 17:03:21 -0300

    attualizacao

--- Links_img/js/first.js
+++ Links_img/js/first.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+let add7 = (add7) => add7 + 7;
+console.log(add7 (9));
+
+let multiply = (a,b) => a*b; 
+console.log(multiply (5,2));
+
+
+function capitalize (change){
+    let upper = change.substr(0,1).toUpperCase() + change.slice(1);
+    return upper;
+}
+console.log(capitalize('abacate'));
+
+function lastLetter (pick){
+    let length = pick.length -1;
+    let last = pick.substr(length,1);
+    return last;
+}
+console.log(lastLetter('vinte um'))

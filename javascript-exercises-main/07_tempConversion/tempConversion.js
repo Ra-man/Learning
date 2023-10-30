815e73ada38a289dc07615e3ab4920e7340fb42b
Rafael Pagnan <rafaelmarquespagnan@gmail.com>
2023-10-30 14:33:30 -0300

    att

--- javascript-exercises-main/07_tempConversion/tempConversion.js
+++ javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFah) {  
+  let  converted = (degreeFah - 32) * (5/9);
+  return parseFloat(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCel) {
+  let converted = degreeCel * (9/5) + 32;
+  return parseFloat(converted.toFixed(1));
 };
 
 // Do not edit below this line

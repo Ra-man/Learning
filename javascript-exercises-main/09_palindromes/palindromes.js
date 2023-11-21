6aac8e598f06515da0a6c4a67116e85a07d19b66
Rafael Pagnan <rafaelmarquespagnan@gmail.com>
2023-11-21 19:45:31 -0300

    Delete javascript-exercises-main directory

--- javascript-exercises-main/09_palindromes/palindromes.js
+++
@@ -1,7 +0,0 @@
-const palindromes = function (pali) {
-    const newPali = pali.toLowerCase().replace(/[^a-z0-9]/g, "");
-    return newPali.split("").reverse().join("") == newPali;
-};
-
-// Do not edit below this line
-module.exports = palindromes;
